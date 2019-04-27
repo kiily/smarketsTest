f3cfe3841ea3c1952545db867c29c0445a091f88
Miguel Marin <miguelmarinvermelho@gmail.com>
2019-04-27 02:16:28 +0100

    feat(app): add basic styles to add, add smarkets logo in navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import logo from '../logo.svg';
+
 import './App.scss';
 
 class App extends Component {
@@ -15,18 +17,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <div className="App-content-container">
+          
+        </div>
       </div>
     );
   }
