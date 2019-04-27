2b5c52c879b724454e3755c90e2214e50fad8b3c
Miguel Marin <miguelmarinvermelho@gmail.com>
2019-04-27 18:42:50 +0100

    feat(eventInfo): build skeleton and add basic css

--- src/components/EventInfo/EventInfo.js
+++ src/components/EventInfo/EventInfo.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './EventInfo.scss';
 
 const eventInfo = (props) => {
 
   return (
     <li className="EventInfo-tile">
-      <div class="info-container">
-        <div class="category-text">
-            
+      <div className="info-container">
+        <div className="category-container">
+          <span className="category-text">{props.sport}</span> &gt; <span className="category-text">{props.competition}</span>
+        </div>
+        <div className="info-title">
+          {props.teams && <div className="teams">
+            <div className="team">
+              <span className="team-name">{props.teams[0]}</span>
+            </div>
+            <div className="team">
+              <span className="team-name">{props.teams[1]}</span>
+            </div>
+          </div>
+          }
+          {props.score && <div className="scores">
+
+          </div>}
         </div>
       </div>
     </li>
   );
+};
+
+eventInfo.propTypes = {
+  sport: PropTypes.string,
+  competition: PropTypes.string,
+  score: PropTypes.array,
+  teams: PropTypes.array
 };
 
 export default eventInfo;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header/Header';
+import EventInfo from '../components/EventInfo/EventInfo';
 
 import requestor from '../utils/requestor';
 
@@ -14,6 +15,7 @@ class App extends Component {
   async componentDidMount() {
     const popularFootballEventRes = await requestor.getPopularEventData('football');
     const popularFootballEventData = popularFootballEventRes.map(res => res.data.events[0]);
+		console.log("TCL: App -> componentDidMount -> popularFootballEventData", popularFootballEventData)
     this.setState({ popularFootballEventData });
   }
 
@@ -22,7 +24,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <div className="content-container">
-
+            <EventInfo sport="football" competition={"Premier"} />
         </div>
       </div>
     );
