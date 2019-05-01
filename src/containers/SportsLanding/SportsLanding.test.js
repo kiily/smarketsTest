6f066b73cac092d6946989638a0e9809173a94f6
Miguel Marin <miguelmarinvermelho@gmail.com>
2019-05-01 09:55:39 +0100

    test(SportsLanding): add tests to SportsLanding component

--- src/containers/SportsLanding/SportsLanding.test.js
+++ src/containers/SportsLanding/SportsLanding.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
 import SportsLanding from './SportsLanding';
+import { BrowserRouter } from 'react-router-dom';
 import { events } from '../../mock-data/event-data';
 
 describe('SportsLanding component', () => {
-  it('should match the snapshot when rendered', () => {
-    const component = create(<SportsLanding currentSport="football" popularEvents={events} />);
+  it('should match the snapshot when rendered before events load', () => {
+    const component = create(<SportsLanding currentSport="football" eventsLoaded={false} />);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+  it('should match the snapshot when rendered after events load', () => {
+    const component = create(<BrowserRouter><SportsLanding currentSport="football" popularEvents={events} eventsLoaded={true} /></BrowserRouter>);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
